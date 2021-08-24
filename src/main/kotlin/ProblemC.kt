1b6a15023b394ff6c23d88926f22c98b2351c791
VYushinsky <78491821+VYushinsky@users.noreply.github.com>
2021-08-24 20:44:27 +0300

    Add files via upload

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,9 +1,17 @@
 class ProblemC {
-
     fun main() {
+        var n = readInt()// объявляем переменные
+        var sum = 0
+        val list: List<Int> = readListInt()
+        val newlist = list.sorted() // сортируем список, нужно для того чтобы расположить
+        // студентов с близким уровнем навыка рядом, чтобы в дальнейшем просуммировать разницу
+        // в уровне навыка между ближайшими студентами
 
-        //ваша реализация в методе main
+        for(i in 1..n step 2){
+            sum += newlist[i] - newlist[i - 1]
+        }
+        println(sum)
+
 
     }
-
 }
